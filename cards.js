7b5b9a2f49f9020ffdab9c1b21e8aeed490bd02f
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-06-22 17:29:35 +0200

    Project Setup

---
+++ cards.js
@@ -0,0 +1,5 @@
+var cards = [];
+
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click', cards)
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function main {
+  var container;
+  var button;
+
+
+
+
+
+  function buildsplash() {
+    var button;
+
+  
+
+    window.getElementById(‘game-container’)
+  }  
+}
+
+window.addEventListener('load', main)
